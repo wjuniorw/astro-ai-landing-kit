1f7d970a736d92fdebc21fdf7bb0f79fd5ad057b
Wilson Junior <wilson.junior90@gmail.com>
2026-09-09 13:30:59 -0300

    chore(commitlint): disable subject-case lint rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 200],
-    'footer-max-line-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-max-line-length': [2, 'always', 100],
+    'subject-case': [0],
   },
 }
